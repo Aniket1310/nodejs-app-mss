7eaf5693d863c27b813c315cacd18cdd1b9aae75
Aniket1310 <77338438+Aniket1310@users.noreply.github.com>
2023-06-18 22:36:44 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.221.74.112:9000/',
+  serverUrl: 'http://3.110.165.133:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.login': 'squ_4a5ead37d823aec01bdf6b86dc60c2f6e071a8cc',
+	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
